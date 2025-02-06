89229fc1bcf0141a38365b313782e073c2b1f493
yijordan <yij@uchicago.edu>
2025-02-06 20:56:09 +0000

    invalid authentication for temp retrieval

--- umbrella.rb
+++ umbrella.rb
@@ -8,6 +8,7 @@ user_location = gets.chomp
 pp user_location
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
+# GEOCODING
 resp = HTTP.get(maps_url)
 raw_response = resp.to_s
 parsed_response = JSON.parse(raw_response)
@@ -15,5 +16,12 @@ results = parsed_response.fetch("results")
 first_result = results.at(0)
 geo = first_result.fetch("geometry")
 loc = geo.fetch("location")
-pp latitude = loc.fetch("lat")
-pp longitude = loc.fetch("lng")
+latitude = loc.fetch("lat")
+longitude = loc.fetch("lng")
+
+# TEMP RETRIEVAL
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + ENV.fetch("PIRATE_WEATHER_KEY") + "/" + latitude.to_s + "," + longitude.to_s
+resp = HTTP.get(pirate_weather_url)
+raw_response = resp.to_s
+parsed_response = JSON.parse(raw_response)
+pp parsed_response
